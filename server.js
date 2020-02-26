54b44f372b0a07fa81b69a09a5c8fe6e9ce229e0
Therachart Wicheanrat <>
2020-02-27 02:25:12 +0700

    setup nodejs server and serve opencv on client side

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const path = require('path')
+
+const PORT = 5000
+
+const app = express()
+
+//views directory
+const viewsDir = path.join(__dirname, 'views')
+app.use(express.static(viewsDir))
+
+// serve css and opencv.js
+app.use(express.static(path.join(__dirname, './public')))
+// serve images content
+app.use(express.static(path.join(__dirname, './images')))
+// server video content
+app.use(express.static(path.join(__dirname, './media')))
+
+// route list
+app.get('/', (req, res) => res.sendFile(path.join(viewsDir, 'index.html')))
+
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`))
